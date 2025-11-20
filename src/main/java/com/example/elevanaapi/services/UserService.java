d6bc5114bb8f5243c2640ecd65951f731e0b7fc5
Halim Choukani <halim.choukani@gmail.com>
2025-11-20 18:48:15 +0100

    Implimenting sign-up logic

--- src/main/java/com/example/elevanaapi/services/UserService.java
+++ src/main/java/com/example/elevanaapi/services/UserService.java
@@ -1,7 +1,32 @@
 package com.example.elevanaapi.services;
 
+import com.example.elevanaapi.models.User;
+import com.example.elevanaapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+
+    public User signUp(String email, String password, String firstName, String lastName) {
+        User user = new User();
+        user.setEmail(email);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setPassword(password);
+        user.setCreatedAt(LocalDateTime.now());
+        if (userRepository.findByEmail((user.getEmail())).isPresent()) {
+            return null;
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
 }
