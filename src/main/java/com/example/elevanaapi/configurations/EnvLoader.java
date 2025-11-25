dc2e8ab46252eb4a037162e3b1459e95ae9b589a
Halim Choukani <halim.choukani@gmail.com>
2025-11-25 07:36:52 +0100

    FIX: Securing env variables and fixing .env file is not readable

--- src/main/java/com/example/elevanaapi/configurations/EnvLoader.java
+++ src/main/java/com/example/elevanaapi/configurations/EnvLoader.java
@@ -7,5 +7,8 @@ public class EnvLoader {
         Dotenv dotenv = Dotenv.configure().load();
         System.setProperty("DataBase_URI", dotenv.get("DataBase_URI"));
         System.setProperty("DataBase_Name", dotenv.get("DataBase_Name"));
+        System.setProperty("CLOUDINARY_NAME", dotenv.get("CLOUDINARY_NAME"));
+        System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+        System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
     }
 }
